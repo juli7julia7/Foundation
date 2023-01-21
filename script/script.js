fcd3b40a961711126c0379370598ec492232d70f
juli7julia7 <106538206+juli7julia7@users.noreply.github.com>
2023-01-21 22:26:21 +0200

    update

--- script/script.js
+++ script/script.js
@@ -3,3 +3,12 @@ $('.hiring__items > div').on('click', function () {
    $(this).parent('.hiring__items').toggleClass('change-color')
    $(this).next('.hiring__items-text').slideToggle(300)
 })
+
+
+$('.features__block').on('click', function (){
+   $(this).children('features__title').toggleClass('.change-color-title');
+   $(this).children('.features__line').toggleClass('.change-features-line')
+   $(this).children('.features__text').slideToggle(300)
+})
+
+
